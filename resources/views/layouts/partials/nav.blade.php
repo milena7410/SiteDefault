857a631b78f84141a47eb30b4d113ca794c94c3a
milena7410 <milenaalegre99@gmail.com>
2022-08-11 20:53:39 -0500

    feat: config layout nav bar

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,27 +1,29 @@
 <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1>Impact<span>.</span></h1>
+
+
+        <img src="/var/www/html/SiteDefault/public/backend/logo-digitar.png" width="150" height="150"/>
+        <!-- <h1>Default<span>.</span></h1> -->
       </a>
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li><a href="#hero">Início</a></li>
+          <li><a href="#about">Secretarias e Governo</a></li>
+          <li><a href="#services">Serviços</a></li>
+          <li><a href="#portfolio">Portal transparência</a></li>
+          <li><a href="#team">COVID-19</a></li>
+          <li><a href="blog.html">Noícias</a></li>
+          <li class="dropdown"><a href="#"><span>Ouvidoria</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+              <li><a href="#">SAC</a></li>
+              <li class="dropdown"><a href="#"><span>Telefones Uteis</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
                 <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
+                  <li><a href="#">Telefone 1</a></li>
+                  <li><a href="#">Telefone 2</a></li>
+                  <li><a href="#">Telefone 3</a></li>
+                  <li><a href="#">Telefone 4</a></li>
+                  <li><a href="#">Telefone 5</a></li>
                 </ul>
               </li>
               <li><a href="#">Drop Down 2</a></li>
@@ -31,4 +33,5 @@
           </li>
           <li><a href="#contact">Contact</a></li>
         </ul>
-      </nav><!-- .navbar -->
+      </nav>
+      <!-- .navbar -->
