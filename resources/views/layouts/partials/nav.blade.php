cab6932879f6f67d80dff4e29855caca17aac7ff
SwarttZ <89858717+SwarttZ@users.noreply.github.com>
2022-08-11 22:49:02 -0400

    fix: close div in nav bar

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,38 +1,44 @@
 <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
+  <a href="index.html" class="logo d-flex align-items-center">
+    <!-- Uncomment the line below if you also wish to use an image logo -->
 
 
-         <!-- <img src="/var/www/html/SiteDefault/public/backend/logo_digitar.png"> -->
+    <!-- <img src="/var/www/html/SiteDefault/public/backend/logo_digitar.png"> -->
 
-        <h1>Digit@r<span>.</span></h1>
-      </a>
-      <nav id="navbar" class="navbar">
+    <h1>Digit@r<span>.</span></h1>
+  </a>
+  <nav id="navbar" class="navbar">
+    <ul>
+      <li><a href="#hero">Início</a></li>
+      <li><a href="#about">Secretarias e Governo</a></li>
+      <li><a href="#services">Serviços</a></li>
+      <li><a href="#portfolio">Portal transparência</a></li>
+      <li><a href="#team">COVID-19</a></li>
+      <li><a href="blog.html">Noícias</a></li>
+      <li class="dropdown"><a href="#"><span>Ouvidoria</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
         <ul>
-          <li><a href="#hero">Início</a></li>
-          <li><a href="#about">Secretarias e Governo</a></li>
-          <li><a href="#services">Serviços</a></li>
-          <li><a href="#portfolio">Portal transparência</a></li>
-          <li><a href="#team">COVID-19</a></li>
-          <li><a href="blog.html">Noícias</a></li>
-          <li class="dropdown"><a href="#"><span>Ouvidoria</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li><a href="#">SAC</a></li>
+          <li class="dropdown"><a href="#"><span>Telefones Uteis</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-              <li><a href="#">SAC</a></li>
-              <li class="dropdown"><a href="#"><span>Telefones Uteis</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Telefone 1</a></li>
-                  <li><a href="#">Telefone 2</a></li>
-                  <li><a href="#">Telefone 3</a></li>
-                  <li><a href="#">Telefone 4</a></li>
-                  <li><a href="#">Telefone 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Telefone 1</a></li>
+              <li><a href="#">Telefone 2</a></li>
+              <li><a href="#">Telefone 3</a></li>
+              <li><a href="#">Telefone 4</a></li>
+              <li><a href="#">Telefone 5</a></li>
             </ul>
           </li>
-          <!-- <li><a href="#contact">Contact</a></li> -->
+          <li><a href="#">Drop Down 2</a></li>
+          <li><a href="#">Drop Down 3</a></li>
+          <li><a href="#">Drop Down 4</a></li>
         </ul>
-      </nav>
-      <!-- .navbar -->
+      </li>
+      <!-- <li><a href="#contact">Contact</a></li> -->
+    </ul>
+  </nav>
+  <!-- .navbar -->
+  <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+  <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+</div>
+</header><!-- End Header -->
+<!-- End Header -->
