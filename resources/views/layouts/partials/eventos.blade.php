6f10605e53eb7a187fb03fd18c723edacaf812d4
milena7410 <milenaalegre99@gmail.com>
2022-08-18 06:02:56 -0500

    confi: section [acesso rapido]

--- resources/views/layouts/partials/eventos.blade.php
+++ resources/views/layouts/partials/eventos.blade.php
@@ -3,90 +3,103 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-header">
-          <h2>Eventos do Mês</h2>
-          <p>Quam sed id excepturi ccusantium dolorem ut quis dolores nisi llum nostrum enim velit qui ut et autem uia reprehenderit sunt deleniti</p>
+          <h2>Acesso Rápido</h2>
+          <!-- <p>Links de fácil acesso</p> -->
         </div>
 
         <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order" data-aos="fade-up" data-aos-delay="100">
 
-          <div>
+          <!-- <div>
             <ul class="portfolio-flters">
               <li data-filter="*" class="filter-active">Todos</li>
               <li data-filter=".filter-app">Semanal</li>
               <li data-filter=".filter-product">teste</li>
               <li data-filter=".filter-branding">teste</li>
               <li data-filter=".filter-books">teste</li>
-            </ul><!-- End Portfolio Filters -->
-          </div>
+            </ul>
+            <-- End Portfolio Filters -->
+          <!-- </div> --> 
 
           <div class="row gy-4 portfolio-container">
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-app">
+
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/app-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/app-1.jpg" class="img-fluid" alt=""></a>
                 <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">App 1</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+                <img src="/var/www/html/SiteDefault/public/frontend/assets/img/new-img/lupa.gif" 
+                style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>Portal Transparencia</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
+            <!-- End Portfolio Item -->
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-product">
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/product-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-1.jpg" class="img-fluid" alt=""></a>
-                <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">Product 1</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+
+                 <div class="portfolio-info">
+                <img src="public/frontend/assets/img/new-img/nf.gif" style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>Nota Fiscal Eletronica</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
-            </div><!-- End Portfolio Item -->
+            </div>
+            <!-- End Portfolio Item -->
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/branding-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-1.jpg" class="img-fluid" alt=""></a>
-                <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">Branding 1</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+              <div class="portfolio-info">
+                <img src="<a target="_blank" href="https://icons8.com/icon/ZiRwjHmdrgtj/informações">Informações</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>" style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>E-Sic</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
             </div><!-- End Portfolio Item -->
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-books">
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/books-1.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/books-1.jpg" class="img-fluid" alt=""></a>
-                <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">Books 1</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+              <div class="portfolio-info">
+                <img src="/var/www/html/SiteDefault/public/frontend/assets/img/new-img/lupa.gif" style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>Processos Licitatórios</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
             </div><!-- End Portfolio Item -->
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-app">
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/app-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/app-2.jpg" class="img-fluid" alt=""></a>
-                <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">App 2</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+              <div class="portfolio-info">
+                <img src="/var/www/html/SiteDefault/public/frontend/assets/img/new-img/lupa.gif" style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>Contas Públicas</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
             </div><!-- End Portfolio Item -->
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-product">
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/product-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/product-2.jpg" class="img-fluid" alt=""></a>
-                <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">Product 2</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+              <div class="portfolio-info">
+                <img src="/var/www/html/SiteDefault/public/frontend/assets/img/new-img/lupa.gif" style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>Holerite On-Line</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
             </div><!-- End Portfolio Item -->
 
             <div class="col-xl-4 col-md-6 portfolio-item filter-branding">
               <div class="portfolio-wrap">
-                <a href="assets/img/portfolio/branding-2.jpg" data-gallery="portfolio-gallery-app" class="glightbox"><img src="assets/img/portfolio/branding-2.jpg" class="img-fluid" alt=""></a>
-                <div class="portfolio-info">
-                  <h4><a href="portfolio-details.html" title="More Details">Branding 2</a></h4>
-                  <p>Lorem ipsum, dolor sit amet consectetur</p>
+              <div class="portfolio-info">
+                <img src="/var/www/html/SiteDefault/public/frontend/assets/img/new-img/lupa.gif" style="width:50px; position: absolute;
+	                left: 270px">
+                  <h4><a>Vagas de Emprego</a></h4>
+                  <p>Clique para acessar</p>
                 </div>
               </div>
             </div><!-- End Portfolio Item -->
