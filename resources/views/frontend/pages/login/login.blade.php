9b3997f2524622eba69e305dc76a6e42ff59cd38
SwarttZ <89858717+SwarttZ@users.noreply.github.com>
2022-08-18 22:14:26 -0400

    feat: create dashboard admin add assets and first steps

--- resources/views/frontend/pages/login/login.blade.php
+++ resources/views/frontend/pages/login/login.blade.php
@@ -72,7 +72,7 @@
 
 <!-- Bootstrap core JavaScript-->
 <script src="{{ asset('frontend/vendor/jquery/jquery.min.js') }}"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('frontend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
 <script src="{{ asset('frontend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
