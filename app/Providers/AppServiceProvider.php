921d7dcd7e4722050e9e7345382b06d75ee5f211
SwarttZ <89858717+SwarttZ@users.noreply.github.com>
2022-10-03 23:27:52 -0400

    fix: error into migrate cloumn photo not null

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       //
+        Schema::defaultStringLength(191);
+
     }
 }
